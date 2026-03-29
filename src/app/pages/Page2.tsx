a0806a11a2b8484fd8ff469e3d1cda362a483c0c
Kim-Carot <nguyenkimngan010106@gmail.com>
2026-03-29 13:23:34 +1100

    Adjust margin for animal images in Page2
    Updated the margin of animal images for better spacing.

--- src/app/pages/Page2.tsx
+++ src/app/pages/Page2.tsx
@@ -109,11 +109,11 @@ export default function Page2() {
 
           {/* BOTTOM - 5 Animal Images in a Row */}
           <div className="flex items-center justify-center pb-4">
-            <img src={foxImg} alt="Fox" className="w-29 h-32 object-contain" style={{ margin: "5px" }} />
-            <img src={duckImg} alt="Duck" className="w-29 h-32 object-contain" style={{ margin: "5px" }} />
-            <img src={mouseWithCupcakeImg} alt="Mouse with Cupcake" className="w-29 h-29 object-contain" style={{ margin: "5px" }} />
-            <img src={miceCakeImg} alt="Mice with Cake" className="w-29 h-29 object-contain" style={{ margin: "5px" }} />
-            <img src={cupcakeStandImg} alt="Cupcake Stand" className="w-29 h-32 object-contain" style={{ margin: "5px" }} />
+            <img src={foxImg} alt="Fox" className="w-29 h-32 object-contain" style={{ margin: "1px" }} />
+            <img src={duckImg} alt="Duck" className="w-29 h-32 object-contain" style={{ margin: "1px" }} />
+            <img src={mouseWithCupcakeImg} alt="Mouse with Cupcake" className="w-29 h-29 object-contain" style={{ margin: "1px" }} />
+            <img src={miceCakeImg} alt="Mice with Cake" className="w-29 h-29 object-contain" style={{ margin: "1px" }} />
+            <img src={cupcakeStandImg} alt="Cupcake Stand" className="w-29 h-32 object-contain" style={{ margin: "1px" }} />
           </div>
         </div>
       </div>
